9e2c5ba8d2df92bc57d2c522c984d66d335aaec1
GUNGODD <chauhanadityac01@gmail.com>
2024-03-01 16:12:58 +0530

    bun and module file

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const nextConfig = {
+    
+    images: {
+        domains: [
+            "uploadthing.com",
+            "utfs.io",
+            "img.clerk.com",
+            "subdomain",
+            "file.string.com",
+        ],
+    },
+    reactStrict: false,
+}
+
+module.exports = nextConfig
